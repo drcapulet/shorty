28f3df7a762baf08dbfb42ddd33e982032925ff8
Alex Coomans <alex@alexcoomans.com>
2009-10-17 20:36:43 -0700

    ... and more

--- lib/shorty/supr.rb
+++ lib/shorty/supr.rb
@@ -29,16 +29,14 @@ module Shorty
     # - url: The long URL you wish to shorten
     # Will return the full url unless you pass false for the second parameter, then it only returns the hash
     def shorten(url, full = true)
-      query = {:longUrl => url}
-      query.merge!(@options)
+      query = self.class.prep_shorten_request(url)
       short = Crack::JSON.parse(self.class.get('/shorten', :query => query))
       self.class.handle_full_or_hash_option(short, url, full)
     end
     
     # self.shorten. see shorten
     def self.shorten(url, full = true)
-      query = {:longUrl => url}
-      query.merge!(@options) if @options
+      query = self.prep_shorten_request(url)
       short = Crack::JSON.parse(get('/shorten', :query => query))
       self.handle_full_or_hash_option(short, url, full)
     end
@@ -47,30 +45,20 @@ module Shorty
     # - shortUrl: he Su.pr URL you wish to expand
     # - hash: The six character hash you wish to expand
     def expand(urlorhash)
-      hash = gsub_url(urlorhash)
-      query = {:hash => hash}
-      query.merge!(@options)
-      expand = self.class.get('/expand', :query => query)
-      expand = Crack::JSON.parse(expand)
+      query, hash = self.class.prep_expand_request(urlorhash)
+      expand = Crack::JSON.parse(self.class.get('/expand', :query => query))
       expand["errorCode"].zero? ? expand["results"][hash]["longUrl"] : self.class.raise_error(expand)
     end
     
     # self.expand. see expand
     def self.expand(urlorhash)
-      hash = gsub_url(urlorhash)
-      query = {:hash => hash}
-      query.merge!(@options) if @options
-      expand = get('/expand', :query => query)
-      expand = Crack::JSON.parse(expand)
+      query, hash = self.prep_expand_request(urlorhash)
+      expand = Crack::JSON.parse(get('/expand', :query => query))
       expand["errorCode"].zero? ? expand["results"][hash]["longUrl"] : self.raise_error(expand)
     end
 
         
     protected
-    
-    def gsub_url(shorturl) 
-      shorturl.split('/').last
-    end
     
     def self.gsub_url(shorturl) 
       shorturl.split('/').last
@@ -89,6 +77,19 @@ module Shorty
       else
         short["errorCode"].zero? ? short["results"][url]["hash"] : self.raise_error(short)
       end
+    end
+    
+    def self.prep_shorten_request(url)
+      query = {:longUrl => url}
+      query.merge!(@options) if @options
+      query
+    end
+    
+    def self.prep_expand_request(urlorhash)
+      hash = self.gsub_url(urlorhash)
+      query = {:hash => hash}
+      query.merge!(@options) if @options
+      [query, hash]
     end
 
   end
